a267f82c44c7ceafe0bbcc8b69cf73a5009f4b55
Spectrethor <1087337@senati.pe>
2023-12-20 20:47:43 -0500

    Agregando un nuevo cambio en el archivo  HelloWord  a la ramaB

--- HelloWord.java
+++ HelloWord.java
@@ -1,7 +1,7 @@
 public class HelloWord{
     public static void main(String[] args)
     {
-        System.out.println("Chanchito Feliz de nuevo");
+        System.out.println("Este cambio lo estamos haciendo para hacer commit en el RamaB");
+        System.out.println("Estamos agregando un texto mas");
     }
-
 }
