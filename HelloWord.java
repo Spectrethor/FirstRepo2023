2aa0039ed5f4d404faabf0717924991b4d238679
Spectrethor <1087337@senati.pe>
2023-12-20 20:21:20 -0500

    Se agrega el archivo .gitIgnore y se modifica la clase HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,7 +1,7 @@
 public class HelloWord{
     public static void main(String[] args)
     {
-        System.out.println("Chanchito Feliz");
+        System.out.println("Chanchito Feliz de nuevo");
     }
 
 }
